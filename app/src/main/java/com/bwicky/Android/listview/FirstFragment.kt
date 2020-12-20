ea8b8fbdca42adcbfff04140bfdd4de217b04813
BimaliWickramasinghe <bimalicw@gmail.com>
2020-12-20 19:55:15 +0530

    post1 view

--- app/src/main/java/com/bwicky/Android/listview/FirstFragment.kt
+++ app/src/main/java/com/bwicky/Android/listview/FirstFragment.kt
@@ -6,14 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.bwicky.Android.listview.posts.api.PostApi
 import com.bwicky.Android.listview.posts.models.Post
+import com.google.gson.GsonBuilder
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 /**
@@ -25,32 +28,35 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://jsonplaceholder.typicode.com/")
-            .build()
+        Log.i("FirstFragment","OnCreateView")
 
-        var postApi= retrofit.create(PostApi::class.java)
-        var postCall = postApi.post;
-        postCall.enqueue(object : Callback<Post> {
-            override fun onFailure(call: Call<Post>, t: Throwable) {
-
-            }
-
-            override fun onResponse(call: Call<Post>, response: Response<Post>) {
-                var title = response.body()!!.title
-                Log.d("FirstFragment",title)
-            }
-        })
 
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val gson = GsonBuilder()
+                .create()
+        // Inflate the layout for this fragment
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        var postApi= retrofit.create(PostApi::class.java)
+        var postCall = postApi.post;
+        postCall.enqueue(object : Callback<Post> {
+            override fun onFailure(call: Call<Post>, t: Throwable) {
+                Log.d("FirstFragment","Error")
+            }
+
+            override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                var title = response.body()!!.title
+                Log.d("FirstFragment","Title is" +title)
+                view.findViewById<TextView>(R.id.title_textview).text = title
+            }
+        })
+
     }
 }
